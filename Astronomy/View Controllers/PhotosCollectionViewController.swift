b40a35a85923be6bffdee456c19f3be9f16d95ab
AustinPotts <pottsevolvements@gmail.com>
2019-11-08 19:31:42 -0500

    Fixed Back & Previous Functions

--- Astronomy/View Controllers/PhotosCollectionViewController.swift
+++ Astronomy/View Controllers/PhotosCollectionViewController.swift
@@ -28,22 +28,28 @@ class PhotosCollectionViewController: UIViewController, UICollectionViewDataSour
     
     @IBAction func goToPreviousSol(_ sender: Any?) {
         guard let solDescriptions = roverInfo?.solDescriptions else { return }
-        guard let sol = solDescription?.sol, sol > 0 else {
+        
+        guard let currentDiscription = solDescription, let index = solDescriptions.firstIndex(of: currentDiscription), index > 0 else {
             solDescription = solDescriptions.first
             return
         }
         
-        solDescription = solDescriptions[sol-1]
+        
+        
+        solDescription = solDescriptions[index - 1]
     }
     
     @IBAction func goToNextSol(_ sender: Any?) {
         guard let solDescriptions = roverInfo?.solDescriptions else { return }
-        guard let sol = solDescription?.sol, sol < solDescriptions.count else {
-            solDescription = solDescriptions.last
-            return
-        }
         
-        solDescription = solDescriptions[sol+1]
+        guard let currentDiscription = solDescription, let index = solDescriptions.firstIndex(of: currentDiscription), index < solDescriptions.count - 1 else {
+                  solDescription = solDescriptions.last
+                  return
+              }
+        
+      
+        
+        solDescription = solDescriptions[index + 1]
     }
     
     // UICollectionViewDataSource/Delegate
